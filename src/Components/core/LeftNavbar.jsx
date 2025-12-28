e76d2a61a2d334ffe2b743be4171208505525663
Jonathan T Cantu <Jonathant.cantu@gmail.com>
2025-12-27 22:41:34 -0600

    added Mock Pages for the website

--- src/Components/core/LeftNavbar.jsx
+++ src/Components/core/LeftNavbar.jsx
@@ -4,7 +4,11 @@ import '../../Styles/leftNavbarCSS.css';
 function LeftNavBar(){
     return (
         <div className="leftNavbar">
-            <h1>This is the left navBar</h1>
+            <h1 className="center">CSS</h1>
+            <ul>
+                <li>css Lesson 1</li>
+                <li>css Lesson 2</li>
+            </ul>
         </div>
         
     )
